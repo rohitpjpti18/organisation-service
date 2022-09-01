3e12ec14dd6a34845a46eede5564eeabd46dc3a8
rohit_prajapati <rohit_prajapati@zohomail.in>
2022-09-01 13:21:38 +0530

    updated constants for app_name

--- src/main/java/wisepanda/common/Constants.java
+++ src/main/java/wisepanda/common/Constants.java
@@ -1,6 +1,8 @@
 package wisepanda.common;
 
 public class Constants {
+    public static final String APP_NAME = "WN_BACKEND_CORE";
+
     public static final String DELETE_BY_QUESTION_NAME              = "QUESTION_NAME";
     public static final String DELETE_BY_QUESTION_ID                = "QUESTION_ID";
     public static final String DELETE_BY_ORGANISATION_ID            = "ORGANISATION_ID";
